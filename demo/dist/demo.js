6baba4d224b1da83e5dd1abd5ac578ec66636351
Antha Auciello <sauciello@panoramaed.com>
2025-09-16 07:03:44 -0400

    navigation-working

--- demo/dist/demo.js
+++ demo/dist/demo.js
@@ -1,6 +1,8 @@
 const dv = docu.dynamicValue;
 const State = docu.State;
 const Demo = {};
+
+// TODO: we will need to fetch source code from the src directory and compiled versions from the dist directory so we probably want the "file" property here to be something more generic like slug (which could be used for hashtag navigation)
 const pages = [{
   name: 'Hello World',
   file: 'hello.jsx'
@@ -14,11 +16,11 @@ const activePage = new State(pages[0]);
 function MenuItem({
   page
 }) {
-  const klass = dv(activePage, p => page === p ? 'active' : '');
+  const klass = dv(activePage, p => page === p ? 'menu-item active' : 'menu-item');
   return docu.jsxEntity("a", {
     className: klass,
     href: "javascript:void(0);",
-    onClick: activePage.set(page)
+    onClick: () => activePage.set(page)
   }, page.name);
 }
 Demo.menu = () => {
@@ -29,5 +31,6 @@ Demo.menu = () => {
   })));
 };
 Demo.page = () => {
+  // TODO: This should check to see if the page object has its "loaded" property set and if not it will need to fetch the source code from src and create script tags pointing to compiled code in dist. Also we may want multiple examples per page so thats an aspect of this to think about..
   return 'bar';
 };
--- demo/src/demo.jsx
+++ demo/src/demo.jsx
@@ -3,6 +3,7 @@ const State = docu.State;
 
 const Demo = {};
 
+// TODO: we will need to fetch source code from the src directory and compiled versions from the dist directory so we probably want the "file" property here to be something more generic like slug (which could be used for hashtag navigation)
 const pages = [
   {
     name: 'Hello World',
@@ -20,14 +21,14 @@ const activePage = new State(pages[0]);
 function MenuItem({ page }) {
   const klass = dv(
     activePage,
-    (p) => page === p ? 'active' : ''
+    (p) => page === p ? 'menu-item active' : 'menu-item'
   );
 
   return (
     <a
       className={klass}
       href="javascript:void(0);"
-      onClick={activePage.set(page)}
+      onClick={() => activePage.set(page)}
     >
       {page.name}
     </a>
@@ -37,11 +38,12 @@ function MenuItem({ page }) {
 Demo.menu = () => {
   return (
     <div className="menu">
-      {	pages.map((page) => MenuItem({ page })) }
+      {	pages.map((page) => <MenuItem page={page} /> }
     </div>
   );
 };
 
 Demo.page = () => {
+  // TODO: This should check to see if the page object has its "loaded" property set and if not it will need to fetch the source code from src and create script tags pointing to compiled code in dist. Also we may want multiple examples per page so thats an aspect of this to think about..
   return 'bar';
 }
